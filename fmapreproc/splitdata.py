dd7fb6a9ef317f164c6cd906208933d42bd2d8d4
ZerocksX <pavao.jerebic@gmail.com>
2020-06-11 12:22:15 +0200

    added genre extraction

--- fmapreproc/splitdata.py
+++ fmapreproc/splitdata.py
@@ -17,7 +17,8 @@ def split_ids(path):
     train = small[tracks['set', 'split'] == 'training']
     test = small[tracks['set', 'split'] == 'test']
     validate = small[tracks['set', 'split'] == 'validation']
-    return np.array(train.index).reshape(-1), np.array(test.index).reshape(-1), np.array(validate.index).reshape(-1),
+    return small, np.array(train.index).reshape(-1), np.array(test.index).reshape(-1), np.array(validate.index).reshape(
+        -1),
 
 
 def split_features(path, train_ids, test_ids, validate_ids):
@@ -36,9 +37,13 @@ def store_to_json(df, path):
     with open(path, 'w+') as file:
         file.write("{")
         n = len(df.index)
-        for i,item_id in enumerate(df.index):
+        for i, item_id in enumerate(df.index):
             file.write(f'"{item_id}": ')
-            file.write(df.loc[item_id].to_json())
+            value = df.loc[item_id]
+            if isinstance(value, pandas.Series):
+                file.write(df.loc[item_id].to_json())
+            else:
+                file.write(json.dumps(value))
             if i < n - 1:
                 file.write(",")
         file.write("}")
@@ -49,20 +54,21 @@ if __name__ == '__main__':
         root = "C:\\Users\\pavao\\Documents\\erasmus\\Courses\\SDM\\assignment3\\fma_metadata\\"
     else:
         root = sys.argv[1]
-    train_ids, test_ids, validate_ids = split_ids(root + "tracks.csv")
-    train, test, validate = split_features(root + "features.csv", train_ids, test_ids, validate_ids)
-    print("train")
-    print(train.shape)
-    # print(train.describe())
-    print("test")
-    print(test.shape)
-    # print(test.describe())
-    print("validate")
-    print(validate.shape)
-    # print(validate.describe())
-    split_root = os.path.dirname(__file__) + "\\split\\"
+    small, train_ids, test_ids, validate_ids = split_ids(root + "tracks.csv")
+    # train, test, validate = split_features(root + "features.csv", train_ids, test_ids, validate_ids)
+    # print("train")
+    # print(train.shape)
+    # # print(train.describe())
+    # print("test")
+    # print(test.shape)
+    # # print(test.describe())
+    # print("validate")
+    # print(validate.shape)
+    # # print(validate.describe())
+    split_root = root + "\\split\\"
     if not os.path.exists(split_root):
         os.makedirs(split_root)
-    store_to_json(train, split_root + "train.json")
-    store_to_json(test, split_root + "test.json")
-    store_to_json(validate, split_root + "validate.json")
+    store_to_json(small.track['genre_top'], split_root + "genres.json")
+    # store_to_json(train, split_root + "train.json")
+    # store_to_json(test, split_root + "test.json")
+    # store_to_json(validate, split_root + "validate.json")

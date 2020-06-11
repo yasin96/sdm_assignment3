e8b05a6c88356e4925cedd8dd14021967fb9b9b1
ZerocksX <pavao.jerebic@gmail.com>
2020-06-11 12:22:38 +0200

    added accuracy calculation

--- src/main/java/LshMain.java
+++ src/main/java/LshMain.java
@@ -1,11 +1,15 @@
+import csv.JsonLoader;
 import distance.ComparatorInterface;
 import distance.DistanceComputer;
 import initialization.Initializer;
 import lombok.extern.slf4j.Slf4j;
 import structures.HashTable;
 import structures.Vector;
+
 import java.io.*;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class LshMain {
@@ -45,26 +49,26 @@ public class LshMain {
     }
 
 
-    private static void initializeAndCalculateHash (List<HashTable> hashtables, int numFeatures, List<Vector> dataset) {
+    private static void initializeAndCalculateHash(List<HashTable> hashtables, int numFeatures, List<Vector> dataset) {
         for (int n = 0; n < numHashTables; n++) {
             HashTable hashTable = new HashTable(createRandomMatrix(hashSize, numFeatures));
             for (Vector vector : dataset) {
                 hashTable.add(vector);
- 
+
             }
             hashtables.add(hashTable);
         }
     }
 
-    private static HashMap<String,List<Vector>> findKNearestNeighbours(List<HashTable> tables, String distanceType,List<Vector> dataset) {
-    	HashMap<String,List<Vector>> neighbours_map=new HashMap<String,List<Vector>>();
-        for (Vector queryVec : dataset) {       	
+    private static HashMap<String, List<Vector>> findKNearestNeighbours(List<HashTable> tables, String distanceType, List<Vector> dataset) {
+        HashMap<String, List<Vector>> neighbours_map = new HashMap<String, List<Vector>>();
+        for (Vector queryVec : dataset) {
             Set<Vector> candidateSet = new HashSet<>();
             for (HashTable table : tables) {
-                List<Vector> v = table.query(queryVec);                 
+                List<Vector> v = table.query(queryVec);
                 candidateSet.addAll(v);
             }
-           
+
 //            System.out.println("---------------------------------------------");
 //            System.out.println("Candidates for vector: " + queryVec.getKey());
 //            candidateSet.forEach(c -> System.out.println(c.getKey()));
@@ -77,36 +81,67 @@ public class LshMain {
             ComparatorInterface dc = new ComparatorInterface(queryVec, measure);
             neighbours.sort(dc);
             if (neighbours.size() > kNeighboursSize) {
-            	neighbours = neighbours.subList(0, kNeighboursSize);
+                neighbours = neighbours.subList(0, kNeighboursSize);
             }
-            neighbours_map.put(queryVec.getKey(),neighbours);
+            neighbours_map.put(queryVec.getKey(), neighbours);
         }
         return neighbours_map;
     }
 
 
-
     public static void main(String[] args) {
         //Initialize train, validation, test datasets
         final Initializer initializer = new Initializer();
-        trainingDataset = initializer.initializeSplitDataSets("fmapreproc\\split\\train.json");
-        validationDataset = initializer.initializeSplitDataSets("fmapreproc\\split\\validate.json");
-        testDataset = initializer.initializeSplitDataSets("fmapreproc\\split\\test.json");
+        trainingDataset = initializer.initializeSplitDataSets("fma_metadata\\split\\train.json");
+        validationDataset = initializer.initializeSplitDataSets("fma_metadata\\split\\validate.json");
+        testDataset = initializer.initializeSplitDataSets("fma_metadata\\split\\test.json");
         //this is needed for "retrain your algorithm with these parameter choices using the training and validation data set as training data"
         trainingAndValidationDataset = new ArrayList<Vector>(trainingDataset);
         trainingAndValidationDataset.addAll(validationDataset);
-   
+
         final int numValidationData = validationDataset.size();
         final int numObservations = trainingDataset.size();
         final int numFeatures = trainingDataset.get(0).getDimensions();
 
         //creation of hash tables
         List<HashTable> tables = new ArrayList<>();
-        initializeAndCalculateHash(tables, numFeatures,trainingDataset);
-        HashMap<String,List<Vector>> neighbours_map=findKNearestNeighbours(tables, "Euclidean",validationDataset);
-      //TODO another function that uses this candidates_map to predict the genre as "the the majority genre of its k-nearestneighbours"
-      //TODO evaluation of the classification accurancy
-      //TODO call initializeAndCalculateHash for trainingAndValidationDataset and findKNearestNeighbours (together with genre prediction) for testDataset
+        initializeAndCalculateHash(tables, numFeatures, trainingDataset);
+        HashMap<String, List<Vector>> neighbours_map = findKNearestNeighbours(tables, "Euclidean", validationDataset);
+        //TODO another function that uses this candidates_map to predict the genre as "the the majority genre of its k-nearestneighbours"
+        //TODO evaluation of the classification accurancy
+        //TODO call initializeAndCalculateHash for trainingAndValidationDataset and findKNearestNeighbours (together with genre prediction) for testDataset
+        System.out.println("Finished finding neighbours");
+        JsonLoader jsonLoader = new JsonLoader();
+        Map<String, String> genres = jsonLoader.loadGenreJson("fma_metadata\\split\\genres.json");
+        Map<String, String> predictedGenres = neighbours_map.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        e -> {
+                            List<String> neighbours = e.getValue().stream().map(vector -> genres.get(vector.getKey())).collect(Collectors.toList());
+                            HashMap<String, Integer> counts = new HashMap<>();
+                            new HashSet<>(neighbours).forEach(s -> counts.put(s, 0));
+                            neighbours.forEach(genre -> counts.put(genre, counts.get(genre) + 1));
+//                            System.out.println(genres.get(e.getKey()));
+//                            System.out.println(neighbours.toString());
+                            String genre;
+                            if (neighbours.isEmpty()) {
+                                genre = "Outlier";
+                            } else {
+                                genre = Collections.max(counts.entrySet(), Map.Entry.comparingByValue()).getKey();
+                            }
+//                            System.out.println(genre);
+                            return genre;
+                        }
+                ));
+        long datasetSize = predictedGenres.size();
+        long correctPredictions = predictedGenres.entrySet().stream().filter(stringStringEntry -> {
+            String trueGenre = genres.get(stringStringEntry.getKey());
+            return trueGenre.equals(stringStringEntry.getValue());
+        }).count();
+        System.out.println(datasetSize);
+        System.out.println(correctPredictions);
+        double accuracy = correctPredictions / (datasetSize * 1.0f);
+        System.out.printf("Accuracy: %s%n", accuracy);
 
     }
 }
--- src/main/java/csv/JsonLoader.java
+++ src/main/java/csv/JsonLoader.java
@@ -13,12 +13,23 @@ import java.util.Map;
 public class JsonLoader {
     private final Gson gson;
     private final Type mapType;
+    private final Type genreMapType;
 
     public JsonLoader() {
         gson = new GsonBuilder()
                 .create();
         mapType = new TypeToken<Map<String, Map<String, Double>>>() {
         }.getType();
+        genreMapType = new TypeToken<Map<String, String>>() {
+        }.getType();
+    }
+
+    public Map<String, String> loadGenreJson(String filepath) {
+        try (Reader reader = new FileReader(filepath)) {
+            return gson.fromJson(reader, genreMapType);
+        } catch (IOException | JsonParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Map<String, Map<String, Double>> loadJson(String filepath) {
